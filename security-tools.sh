30b41defea2820b74c9590006e3af561dc2812b7
Faheem Khan <faheem5948@gmail.com>
2023-10-31 08:11:58 +0000

    renamed to setup.sh
    Signed-off-by: GitHub <noreply@github.com>

--- security-tools.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "== Installing Gitleaks(github.com/gitleaks/gitleaks) =="
-wget https://github.com/gitleaks/gitleaks/releases/download/v8.18.0/gitleaks_8.18.0_linux_x64.tar.gz -O /tmp/gitleaks.tar.gz;
-tar -xzvf /tmp/gitleaks.tar.gz
-mv gitleaks /usr/local/bin
-
-# EOF
